c2b6f5b27f01385442af829a52cee650d4a8dc65
asgeek <37499060+asgeek@users.noreply.github.com>
2018-12-29 08:51:32 +0530

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -5,9 +5,15 @@ import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.script
 
 object TestBc : BuildType({
     name = "Test_BC"
+    val vcsRoot = GitVcsRoot({
+        uuid = "vcs_python"
+        id = "TestBc_VcsRoot"
+        name = "Example of DSL VCS Root"
+        url = "https://github.com/asgeek/webscraper-python.git"
+    })
 
     vcs {
-        root(DslContext.settingsRoot)
+        root(vcsRoot)
     }
 
     steps {
